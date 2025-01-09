2296dc34ee04a65e7a3fcdfbb3947ef5fce78b84
Ryan French <ryanjfre@amazon.co.uk>
2025-01-09 16:43:57 +0000

    Add local gateway context provider and query

--- lib/cloud-assembly/context-queries.ts
+++ lib/cloud-assembly/context-queries.ts
@@ -55,6 +55,11 @@ export enum ContextProvider {
   KEY_PROVIDER = 'key-provider',
 
   /**
+   * Local Gateway Provider
+   */
+  LOCAL_GATEWAY_PROVIDER = 'local-gateway',
+
+  /**
    * A plugin provider (the actual plugin name will be in the properties)
    */
   PLUGIN = 'plugin',
@@ -348,6 +353,20 @@ export interface KeyContextQuery extends ContextLookupRoleOptions {
 }
 
 /**
+ * Query input for looking up a Local Gateway
+ */
+export interface LocalGatewayContextQuery extends ContextLookupRoleOptions {
+  /**
+   * Filters to apply to the Local Gateway
+   *
+   * Filter parameters are the same as passed to DescribeLocalGateways.
+   *
+   * @see https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/ec2/command/DescribeLocalGatewaysCommand/
+   */
+  readonly filter: { [key: string]: string };
+}
+
+/**
  * Query input for plugins
  *
  * This alternate branch is necessary because it needs to be able to escape all type checking
@@ -380,4 +399,5 @@ export type ContextQueryProperties =
   | LoadBalancerListenerContextQuery
   | SecurityGroupContextQuery
   | KeyContextQuery
+  | LocalGatewayContextQuery
   | PluginContextQuery;
